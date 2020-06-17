10082fb7a2c0e98f763aa0027c82d4f668b366b6
ellavrubel <webella@protonmail.com>
2020-06-17 14:31:57 +0300

    participation finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,14 @@
 
             $(function () {
-             $('.list__text .list__text-item').on('click', function () {
+             $('.list__text .list__text-item, .reviews__tabs .reviews__tab').on('click', function () {
 
                  let id = $(this).attr('data-id');
 
-                 $('.list__content').find('.list-item').removeClass('active-list').hide();
-                 $('.list__text .tabs').find('.list__text-item').removeClass('active');
+                 $('.list__content, .reviews__container-text').find('.list-item, .reviews__content-item').removeClass('active-list', 'active-tab').hide();
+                 $('.list__text .tabs, .reviews__content').find('.list__text-item, .reviews__tab').removeClass('active');
 
                  $(this).addClass('active');
-                 $('#'+id).addClass('active-list').fadeIn();
+                 $('#'+id).addClass('active-list','active-tab').fadeIn();
 
                  return false;
 
